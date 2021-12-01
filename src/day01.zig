aaa197fdeb9a0403ada0c80e793b0c29160dc462
Basile Henry <bjm.henry@gmail.com>
2021-12-01 13:26:30 +0100

    Simpler window day 01

--- src/day01.zig
+++ src/day01.zig
@@ -4,10 +4,12 @@ const print = std.debug.print;
 const data = @embedFile("../inputs/day01.txt");
 
 pub fn main() anyerror!void {
+    // Part 1
     var prev: ?usize = null;
     var increased: usize = 0;
 
-    var prev_window: [3]?usize = [3]?usize{ null, null, null };
+    // Part 2
+    var prev_window: [3]usize = undefined;
     var prev_window_index: usize = 0;
     var increased_window: usize = 0;
 
@@ -15,6 +17,7 @@ pub fn main() anyerror!void {
     while (lines.next()) |line| {
         const depth = try std.fmt.parseInt(usize, line, 10);
 
+        // Part 1
         if (prev) |p| {
             if (depth > p) {
                 increased += 1;
@@ -23,12 +26,17 @@ pub fn main() anyerror!void {
 
         prev = depth;
 
-        var prev_sum = sum_opt(prev_window[0..]);
-        prev_window[@mod(prev_window_index, 3)] = depth;
-        var cur_sum = sum_opt(prev_window[0..]);
+        // Part 2
+        if (prev_window_index >= 3) {
+            var prev_sum = sum(prev_window[0..]);
+            prev_window[@mod(prev_window_index, 3)] = depth;
+            var cur_sum = sum(prev_window[0..]);
 
-        if (prev_window_index >= 3 and cur_sum.? > prev_sum.?) {
-            increased_window += 1;
+            if (cur_sum > prev_sum) {
+                increased_window += 1;
+            }
+        } else {
+            prev_window[@mod(prev_window_index, 3)] = depth;
         }
 
         prev_window_index += 1;
@@ -38,15 +46,11 @@ pub fn main() anyerror!void {
     print("Part 2: {}\n", .{increased_window});
 }
 
-fn sum_opt(xs: []?usize) ?usize {
+fn sum(xs: []usize) usize {
     var out: usize = 0;
 
-    for (xs) |ox| {
-        if (ox) |x| {
-            out += x;
-        } else {
-            return null;
-        }
+    for (xs) |x| {
+        out += x;
     }
 
     return out;
