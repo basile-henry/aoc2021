95a49b6b3008f1137cd3c505bc475019bec53d48
Basile Henry <bjm.henry@gmail.com>
2021-12-10 09:38:59 +0100

    Time each day in main

--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,9 @@ pub fn main() anyerror!void {
     var fixed_alloc = std.heap.FixedBufferAllocator.init(alloc_buffer[0..]);
     const allocator = &fixed_alloc.allocator;
 
+    var timer = try std.time.Timer.start();
+    var times = std.ArrayList(u64).init(allocator);
+
     const last_day = 10;
 
     comptime var day: u8 = 1;
@@ -18,5 +21,18 @@ pub fn main() anyerror!void {
 
         print("\nDay " ++ day_str ++ "\n", .{});
         try @import("./day" ++ day_str ++ ".zig").main();
+
+        try times.append(timer.lap());
     }
+
+    print("\nTimes:\n", .{});
+    var total_time: u64 = 0;
+    for (times.items) |time, i| {
+        total_time += time;
+        const t: f64 = @intToFloat(f64, time) / 1_000_000; // nano to milli
+        print("  day {d:0>2}: {d: >6.3}ms\n", .{ i + 1, t });
+    }
+
+    const total: f64 = @intToFloat(f64, total_time) / 1_000_000; // nano to milli
+    print("  total:  {d: >6.3}ms\n", .{total});
 }
